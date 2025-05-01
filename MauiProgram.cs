9507451b5be54d6f26396b82334517a98a4a92e7
Dalton Smith <daltz@ctr-electronics.com>
2025-05-01 13:03:33 -0400

    Add use skiasharp. Issue still reproduces

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace MauiSkiaCrashRepro
 {
@@ -9,6 +10,7 @@ namespace MauiSkiaCrashRepro
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
